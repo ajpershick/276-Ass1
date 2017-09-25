75f0867972f14bdc5e4093e6a0ff127c90453de4
ajpershick <ajpershick@gmail.com>
2017-09-25 14:04:32 -0700

    make histogram.html the homepage

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def hello
-    render html: "hello, world!"
+    render file: 'public/histogram.html', layout: false
   end
 end
