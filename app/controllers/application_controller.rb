82ecee8bb1d03451dbf696c3f1f76ed7d1a4cbab
ajpershick <ajpershick@gmail.com>
2017-09-25 22:49:07 -0700

    Layout of Histogram.html good to go

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def hello
-    render file: 'public/histogram.html', layout: false
+    render file: 'public/histogram.html'
   end
 end
+
